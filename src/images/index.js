64e7562c4d12b4ec42cdd99b77181cbdbf23a16e
Matt Zuckermann <mczuckermann@gmail.com>
2019-04-19 19:36:13 -0700

    Adjusted styling and project order

--- src/images/index.js
+++ src/images/index.js
@@ -1,9 +1,9 @@
 export default {
-  image1: require("./jz-music-portfolio.jpg"),
+  image1: require("./codesnippetsScreenShot.jpg"),
   image2: require("./bestSellerSearcher.jpg"),
-  image3: require("./codesnippetsScreenShot.jpg"),
+  image3: require("./jz-music-portfolio.jpg"),
   image4: require("./storestockScreenShot.jpg"),
-  image5: require("./liribotScreenShot.jpg"),
-  image6: require("./friendScreenShot.jpg"),
+  image5: require("./friendScreenShot.jpg"),
+  image6: require("./liribotScreenShot.jpg"),
   image7: require("./zuckermannHeadShot.png"),
 }
--- src/json/imageInfo.js
+++ src/json/imageInfo.js
@@ -1,10 +1,11 @@
 export default [
   {
-    appName: `Music Portfolio`,
-    deployLink: `https://jz-development.netlify.com/`,
-    repoLink: `https://github.com/mczuckermann/jz-music-portfolio`,
-    shortBio: `React, Gatsby, GraphQL, Cloudinary, Typescript, Jest`,
-    projectRole: `Sole Developer`,
+    appName: `Snippets`,
+    imageLink: `codesnippetsScreenShot.jpg`,
+    deployLink: `https://morning-harbor-79094.herokuapp.com/home`,
+    repoLink: `https://github.com/mczuckermann/CodeSnippets`,
+    shortBio: `Handlebars, Javascript, Node.js, Express.js, Passport.js, MySQL, Sequelize`,
+    projectRole: `Authentication // Front-End Developer`,
   },
   {
     appName: `Best Seller Searcher`,
@@ -15,12 +16,11 @@ export default [
     projectRole: `Authentication // API // Back-End Developer`,
   },
   {
-    appName: `Snippets`,
-    imageLink: `codesnippetsScreenShot.jpg`,
-    deployLink: `https://morning-harbor-79094.herokuapp.com/home`,
-    repoLink: `https://github.com/mczuckermann/CodeSnippets`,
-    shortBio: `Handlebars, Javascript, Node.js, Express.js, Passport.js, MySQL, Sequelize`,
-    projectRole: `Authentication // Front-End Developer`,
+    appName: `Music Portfolio`,
+    deployLink: `https://jz-development.netlify.com/`,
+    repoLink: `https://github.com/mczuckermann/jz-music-portfolio`,
+    shortBio: `React, Gatsby, GraphQL, Cloudinary, Typescript, Jest`,
+    projectRole: `Sole Developer`,
   },
   {
     appName: `Store Stock`,
@@ -31,19 +31,19 @@ export default [
     projectRole: `Sole Developer`,
   },
   {
-    appName: `Liri Bot`,
-    imageLink: `liribotScreenShot.jpg`,
-    deployLink: `https://www.youtube.com/watch?v=8cOBg6EDE40`,
-    repoLink: `https://github.com/mczuckermann/Liri-Bot_Node-Javascript-RestAPIs`,
-    shortBio: `JavaScript, Node.js`,
-    projectRole: `Sole Developer`,
-  },
-  {
     appName: `Friend Finder`,
     imageLink: `friendScreenShot.jpg`,
     deployLink: `https://friend-finder-zuckermann.herokuapp.com/`,
     repoLink: `https://github.com/mczuckermann/Friend-Finder_Express-Node-Javascript`,
     shortBio: `JavaScript`,
+    projectRole: `Sole Developer`,
+  },
+  {
+    appName: `Liri Bot`,
+    imageLink: `liribotScreenShot.jpg`,
+    deployLink: `https://www.youtube.com/watch?v=8cOBg6EDE40`,
+    repoLink: `https://github.com/mczuckermann/Liri-Bot_Node-Javascript-RestAPIs`,
+    shortBio: `JavaScript, Node.js`,
     projectRole: `Sole Developer`,
   },
 ]
