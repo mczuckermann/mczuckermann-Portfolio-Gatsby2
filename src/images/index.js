55847c63ebcf634d07d487780afa2139efea1d85
Matt Zuckermann <mczuckermann@gmail.com>
2019-05-12 22:23:52 -0700

    Editted naming of images again

--- src/images/index.js
+++ src/images/index.js
@@ -1,7 +1,7 @@
 export default {
   image1: require("./jzMusicPortfolio.jpg"),
   image2: require("./bestSellerSearcher.jpg"),
-  image3: require("./codeSnippetsScreenShot.jpg"),
+  image3: require("./codesnippetsScreenShot.jpg"),
   image4: require("./storestockScreenShot.jpg"),
   image5: require("./liribotScreenShot.jpg"),
   image6: require("./friendScreenShot.jpg"),
