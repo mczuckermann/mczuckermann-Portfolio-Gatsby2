09e5b6411213b91b5edd02d92ad07aa8640ccc78
Matt Zuckermann <mczuckermann@gmail.com>
2019-05-12 22:30:29 -0700

    Adjusted naming another time

--- src/images/index.js
+++ src/images/index.js
@@ -2,8 +2,8 @@ export default {
   image1: require("./jzMusicPortfolio.jpg"),
   image2: require("./bestSellerSearcher.jpg"),
   image3: require("./codesnippetsScreenShot.jpg"),
-  image4: require("./storestockScreenShot.jpg"),
-  image5: require("./liribotScreenShot.jpg"),
+  image4: require("./storestockItemGrabberScreenShot.jpg"),
+  image5: require("./liribotCodeScreenShot.jpg"),
   image6: require("./friendScreenShot.jpg"),
   image7: require("./zuckermannHeadShot.png"),
 }
