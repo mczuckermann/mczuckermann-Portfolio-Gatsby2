f548b50a43862be63d7303314bba21f3d1608ffd
Matt Zuckermann <mczuckermann@gmail.com>
2019-10-09 19:00:51 -0700

    Added SEO image

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
@@ -46,6 +39,10 @@ function SEO({ description, lang, meta, keywords, title }) {
         {
           property: `og:description`,
           content: metaDescription,
+        },
+        {
+          property: `og:image`,
+          content: `https://media.licdn.com/dms/image/C4E03AQECnHtBedEWvw/profile-displayphoto-shrink_200_200/0?e=1576108800&v=beta&t=d8kwanNGn1uORcsIEQ2zt1vXaOnqPHPo-XG6PNB7fVY`,
         },
         {
           property: `og:type`,
