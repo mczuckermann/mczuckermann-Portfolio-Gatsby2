8c0bef8db83d6a676fa529dc4f5d879813f1c486
Matt Zuckermann <mczuckermann@gmail.com>
2020-05-08 10:02:07 -0700

    Adjusted animation triggers and css

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { useSpring, animated, config } from "react-spring"
 import { Waypoint } from "react-waypoint"
 import { GlobalContext } from "../../pages"
@@ -6,16 +6,32 @@ import "./home.css"
 
 const Home = () => {
   const { setValue, allRefs } = useContext(GlobalContext)
+  const [on, toggle] = useState(true)
+  const fadeInOut = useSpring({
+    opacity: on ? 1 : 0.3,
+    transform: on ? "scale(1, 1)" : "scale(0.85,0.95)",
+    config: config.molasses,
+  })
   const fade = useSpring({
     from: { opacity: 0 },
     opacity: 1,
     config: config.molasses,
   })
+
   return (
-    <div>
+    <div ref={allRefs[0]}>
       <Waypoint bottomOffset="0%" onEnter={() => setValue(0)} />
-      <animated.div ref={allRefs[0]} style={fade} className="homeBody" />
-      <animated.div style={fade} className="home-title">
+      <animated.div style={fade} className="homeBody" />
+      <Waypoint
+        bottomOffset="50%"
+        onEnter={() => {
+          if (on) toggle(false)
+        }}
+        onLeave={() => {
+          if (!on) toggle(true)
+        }}
+      />
+      <animated.div style={fadeInOut} className="home-title">
         Matt Zuckermann
       </animated.div>
       <animated.div style={fade} className="arrow">
