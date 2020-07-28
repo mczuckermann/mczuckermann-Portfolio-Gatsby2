0ce91b10205883c7510af5721c406a8653a246ee
Matt Zuckermann <mczuckermann@gmail.com>
2020-07-28 08:06:16 -0700

    added resume and businesscard routes for career documents

---
+++ src/pages/resume.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import GoogleDocsViewer from "react-google-docs-viewer"
+
+const Resume = () => {
+  return (
+    <GoogleDocsViewer
+      width="100vw"
+      height="100vh"
+      fileUrl="https://res.cloudinary.com/joshzuckermann-netlify-com/image/upload/v1595947522/Matt_Zuckermann_Resume.pdf"
+    />
+  )
+}
+
+export default Resume
