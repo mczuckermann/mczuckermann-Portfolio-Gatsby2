d26d2528b8afff75d53ebae71bb2fd08d8edcd55
Matt Zuckermann <matt@mattzuckermann.dev>
2020-11-11 18:45:45 -0800

    Updated demo credential components

--- src/components/DemoLineNYT/index.jsx
+++ src/components/DemoLineNYT/index.jsx
@@ -4,19 +4,7 @@ const DemoLineNYT = () => {
   return (
     <div className="demoNYT">
       <div className="demoBody">
-        *For a demo version of{" "}
-        <i>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="demoPassLinks"
-            href="https://www.nytsearcher.com/"
-          >
-            NYT Searcher
-          </a>
-        </i>
-        , use the username "<span className="demo">demo@demo.com</span>" with
-        the password "<span className="demo">demo</span>
+        <span className="demo">demo@demo.com // demo</span>
       </div>
     </div>
   )
--- src/components/DemoLineSnippets/index.jsx
+++ src/components/DemoLineSnippets/index.jsx
@@ -3,22 +3,8 @@ import React from "react"
 const DemoLineSnippets = () => {
   return (
     <div className="demoSnippets">
-      <div
-        className="demoBody"
-      >
-        *For a demo version of{" "}
-        <i>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="demoPassLinks"
-            href="https://www.codesnippets.app/"
-          >
-            Snippets.
-          </a>
-        </i>
-        , use the username "<span className="demo">demo@demo.com</span>" with the password "<span className="demo">demo</span>
-        "
+      <div className="demoBody">
+        <span className="demo">demo@demo.com // demo</span>
       </div>
     </div>
   )
